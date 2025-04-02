cd0f1930138cd9d204c2a47c945828f89f418561
Sahil-0701 <kaushiksr868@gmail.com>
2025-04-03 00:45:40 +1100

    feat(Add Api to home page and navigator for player with display relevent video)

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,14 +1,31 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
+import { Link } from "react-router-dom";
 
 const TitleCards = ({title, category}) => {
+  const [apiData, setApiData] = useState([]);
+
   const cardsRef = useRef();
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZGVkNGY3YTJmMTRkOGRiODRlYmNhMTRkZTkyZGExZCIsIm5iZiI6MTc0MzUxMDMxOC44MjcwMDAxLCJzdWIiOiI2N2ViZGIyZTUxZjYyNTRmNzY4YmY0YjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.npete7gMYqao7Dd27kYpTH_Pov1bMOAKQsvNUvGeUV0'
+    }
+  };
+  
+  
   const handelWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
   useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handelWheel);
   }, []);
 
@@ -16,12 +33,12 @@ const TitleCards = ({title, category}) => {
     <div className="title-cards">
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
-            <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
-            </div>
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+              <p>{card.original_title }</p>
+            </Link>
           );
         })}
       </div>
